5f3f9947e083055e58c133269bd5804d72e52a78
gp2112 <me@guip.dev>
2022-05-30 09:44:23 -0300

    calculo bit paridade

--- main.ino
+++ main.ino
@@ -4,11 +4,24 @@
 #define BAUD_RATE 1
 #define HALF_BAUD 1000/(2*BAUD_RATE)
 
+
+// PAR -> 0 | IMPAR -> 1
+#define PARITY 0 
+
 #include "Temporizador.h"
+
+
 
 // Calcula bit de paridade - Par ou impar
 bool bitParidade(char dado){
-
+    int count = 0;
+    char div = dado;
+    
+    while (div>0) {
+        count += div%2; // se div for impar: div%2=1. Se for par: div%2=0
+        div = div >> 1; // bit-shift a direita = divisao inteira por 2
+    }
+    return count%2==0;
 }
 
 // Rotina de interrupcao do timer1
