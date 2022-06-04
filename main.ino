11f4cb2bb2afa12561847e2eecd9a125b18e103f
gp2112 <me@guip.dev>
2022-06-04 03:12:39 -0300

    funcoes de wrrite/rread clk,rts,cts,data

--- main.ino
+++ main.ino
@@ -1,6 +1,8 @@
 
-#define PINO_RX 13
-#define PINO_TX 13
+#define PINO_TX 13 // Pin de transmissão
+#define PINO_CTS 12
+#define PINO_RTS 11
+#define PINO_CLK 10
 #define BAUD_RATE 1
 #define HALF_BAUD 1000/(2*BAUD_RATE)
 
@@ -10,19 +12,10 @@
 
 #include "Temporizador.h"
 
+char clock = 0,
+     sig_rts = 0,
+     sig_cts = 0;
 
-
-// Calcula bit de paridade - Par ou impar
-bool bitParidade(char dado){
-    int count = 0;
-    char div = dado;
-    
-    while (div>0) {
-        count += div%2; // se div for impar: div%2=1. Se for par: div%2=0
-        div = div >> 1; // bit-shift a direita = divisao inteira por 2
-    }
-    return count%2==0;
-}
 
 // Rotina de interrupcao do timer1
 // O que fazer toda vez que 1s passou?
@@ -30,18 +23,51 @@ ISR(TIMER1_COMPA_vect){
   //>>>> Codigo Aqui <<<<
 }
 
+void transmitBit(char b) {
+    digitalWrite(PINO_TX, b ? HIGH : LOW);
+}
+
+void transmitCLK() {
+    digitalWrite(PINO_CLK, clock ? HIGH : LOW);
+    clock = !clock;
+}
+
+void sendData(char data) {
+    int n=0, ones=0;
+    int div = data;
+    while (div>0 || n < 8) {
+        transmitBit(div%2);
+        ones += div%2;
+        div = div >> 1;
+        n++;
+    }
+    transmitBit(ones%2==PARITY);
+
+}
+
+void readCTS() {
+    sig_cts = digitalRead(PINO_CTS)==HIGH;
+}
+
+void sendRTS() {
+    digitalWrite(PINO_RTS, sig_rts ? HIGH : LOW);
+}
+
 // Executada uma vez quando o Arduino reseta
 void setup(){
-  //desabilita interrupcoes
-  noInterrupts();
-  // Configura porta serial (Serial Monitor - Ctrl + Shift + M)
-  Serial.begin(9600);
-  // Inicializa TX ou RX
-  //>>>> Codigo Aqui <<<<
-  // Configura timer
-  //>>>> Codigo Aqui <<<<
-  // habilita interrupcoes
-  interrupts();
+    //desabilita interrupcoes
+    noInterrupts();
+    // Configura porta serial (Serial Monitor - Ctrl + Shift + M)
+    Serial.begin(9600);
+    // Inicializa pins
+    pinMode(PINO_TX, OUTPUT);
+    pinMode(PINO_RTS, OUTPUT);
+    pinMode(PINO_CTS, INPUT);
+    pinMode(PINO_CLK, OUTPUT);
+    // Configura timer
+    configuraTemporizador(BAUD_RATE);
+    // habilita interrupcoes
+    interrupts();
 }
 
 // O loop() eh executado continuamente (como um while(true))
